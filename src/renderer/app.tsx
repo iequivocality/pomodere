a8283cc1746f7e5b7ce3251b876db130a0da2c5d
iequivocality <xanthiandere@gmail.com>
2021-12-16 22:19:59 +0800

    initial tailwind + react + electron

---
+++ src/renderer/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function render() {
+  ReactDOM.render(<h1 className="text-3xl font-bold underline">
+  Hello world!
+</h1>, document.body);
+}
+
+render();
